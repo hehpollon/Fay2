c1bd08b443d873248ba1e1fc62868be9477ac503
terrykwon <terrykkwon@gmail.com>
2017-02-04 21:58:00 +0900

    Initial commit

---
+++ demo/src/main/java/com/unithon/android/fay/demo/CameraFragment.java
@@ -0,0 +1,10 @@
+package com.unithon.android.fay.demo;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by terrykwon on 04/02/2017.
+ */
+
+public class CameraFragment extends Fragment{
+}
